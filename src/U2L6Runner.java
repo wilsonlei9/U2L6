1736271100f0822775f44f987d29be084ee6ecc3
BT_4N2_22 <wlei6251@bths.edu>
2022-10-04 12:32:23 -0400

    Initial commit

---
+++ src/U2L6Runner.java
@@ -0,0 +1,11 @@
+public class U2L6Runner {
+    public static void main(String[] args) {
+        String name = "Angie";
+        int age = 17;
+        String word = "Bam";
+        String info = "Name:\n" + name + "\n" + "\nAge:\n" + age;
+        info += "\n\nFavorite Word:\n" +"\"" + word + "!\"";
+        System.out.println(info);
+
+    }
+}
